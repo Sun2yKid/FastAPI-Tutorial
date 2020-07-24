6ac8d51d3ba6bce99d011a4f1a196c668baa0f77
zhonghui.yao <zhonghua00700@gmail.com>
2020-07-24 16:39:58 +0800

    Body - Multiple Parameters

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from typing import Optional, List
 
-from fastapi import FastAPI, Query, Path
+from fastapi import FastAPI, Query, Path, Body
 from pydantic import BaseModel
 
 
@@ -136,4 +136,32 @@ async def read_items(
     results = {"item_id": item_id}
     if q:
         results.update({"q": q})
+    return results
+
+
+@app.put("/items/{item_id}")
+async def update_item(
+        *,
+        item_id: int = Path(..., title="The ID of the item to get", ge=0, le=1000),
+        q: Optional[str] = None,
+        item: Optional[Item] = None,
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    if item:
+        results.update({"item": item})
+    return results
+
+
+class User(BaseModel):
+    username: str
+    full_name: Optional[str] = None
+
+
+@app.put("/items/{item_id}")
+async def update_item(
+        item_id: int, item: Item, user: User, importance: int = Body(...)
+):
+    results = {"item_id": item_id, "item": item, "user": user, "importance": importance}
     return results
